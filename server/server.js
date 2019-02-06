2881c3be1a228007baaa9b193d90f0ce2b8e78f7
JuliaAugustSugarman <jules.august@gmail.com>
2019-02-06 14:03:06 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+console.log('create a server project');
+
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('server/public'));
+
+app.get('/quotes', function (req, res) {
+    res.send(quotes_data);
+});
+
+let quotes_data = [
+    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+]
+
+
+
+
+
+
+
+app.listen(5000, function () {
+    console.log('running on port 5000');
+
+});
+
+
