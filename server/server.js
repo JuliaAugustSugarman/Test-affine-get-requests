a1482910d203dd9e020acad087a176133044d87f
JuliaAugustSugarman <jules.august@gmail.com>
2019-02-06 15:11:22 -0600

    p

--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
-console.log('create a server project');
+console.log('server js is running');
 
 let express = require('express');
-
+let PORT = 5000
 let app = express();
 
 app.use(express.static('server/public'));
-
-app.get('/quotes', function (req, res) {
-    res.send(quotes_data);
-});
 
 let quotes_data = [
     { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
     { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
     { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+
 ]
 
 
+app.get('/quotes', (req, res) => {
+    res.send(quotes_data);
+});
 
 
-
-
-
-app.listen(5000, function () {
-    console.log('running on port 5000');
-
+app.listen(PORT, () => {
+    console.log('running on port', PORT);
 });
 
 
